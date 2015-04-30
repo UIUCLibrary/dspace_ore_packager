b663a04b0d8ac0b89a877f7bdcab11aad3da3c32
Nushrat Jahan Khan <njkhan2@illinois.edu>
2015-04-30 14:12:52 -0500

    Metadata extraction and restclient sections integrated

--- lib/dspace_ore_packager/package.rb
+++ lib/dspace_ore_packager/package.rb
@@ -1,21 +1,27 @@
+require 'rubygems'
 require 'nokogiri'
 require 'rest_client'
 require 'json'
+require 'highline/import'
 
 module DspaceOrePackager
   class Package
     attr_reader :file, :document, :ars
     def initialize(ore_xml)
 
+      @host = 'http://localhost:8080'
+      @user = 'njkhan505@gmail.com'
+      @pwd = '123456'
+
       @file = File.new(ore_xml)
       @aggregation_uri='http://www.openarchives.org/ore/terms/Aggregation'
       @document = Nokogiri::XML(@file)
-      @agg =  @document.xpath("//rdf:Description[rdf:type/@rdf:resource='#{@aggregation_uri}']")
       @agg_id = @document.xpath("//rdf:Description[rdf:type/@rdf:resource='#{@aggregation_uri}']/@rdf:about")
-      @ar_ids= @document.xpath("//rdf:Description[rdf:type/@rdf:resource='#{@aggregation_uri}']/ore:aggregates")
-      @ars = @document.xpath("//rdf:Description[ore:isAggregatedBy='#{@agg_id}']")
-      @agg_metadata_id = @document.xpath("//rdf:Description[rdf:type/@rdf:resource='#{@aggregation_uri}']/ore:isDescribedBy/@rdf:resource")
       @agg_dcterms = @document.xpath("//rdf:Description[ore:describes/@rdf:resource='#{@agg_id}']/*[starts-with(name(),'dcterms:')]")
+      # @agg =  @document.xpath("//rdf:Description[rdf:type/@rdf:resource='#{@aggregation_uri}']")
+      # @ar_ids= @document.xpath("//rdf:Description[rdf:type/@rdf:resource='#{@aggregation_uri}']/ore:aggregates")
+      # @ars = @document.xpath("//rdf:Description[ore:isAggregatedBy='#{@agg_id}']")
+      # @agg_metadata_id = @document.xpath("//rdf:Description[rdf:type/@rdf:resource='#{@aggregation_uri}']/ore:isDescribedBy/@rdf:resource")
 
       @key = Array.new
       @value = Array.new
@@ -30,15 +36,54 @@ module DspaceOrePackager
 
     def processAgg
 
+      #Login
+      begin
+        response = RestClient.post("#{@host}/rest/login", {"email" => "#{@user}", "password" => "#{@pwd}"}.to_json,
+                                   {:content_type => 'application/json',
+                                    :accept => 'application/json'})
+        login_token = response.to_str
+        puts "Your login token is: #{login_token}"
+
+      rescue => e
+        puts "ERROR: #{e}"
+      end
+
+      # Retrieve collection id to create an item
+      handle_id = ask("Enter handle ID of the collection: ")
+      begin
+        collection = RestClient.get("#{@host}/rest/handle/2142/#{handle_id}")
+        doc = Nokogiri::XML(collection)
+        getid = doc.xpath("//collection/id")
+        collectionid = "#{getid}"[/.*>(.*)</,1]
+        puts "collection id is: #{collectionid}"
+      end
+
+      #Create an item
+      puts 'Creating an item.'
+      begin
+        item = RestClient.post("#{@host}/rest/collections/#{collectionid}/items",{"type" => "item"}.to_json,
+                               {:content_type => 'application/json', :accept => 'application/json', :rest_dspace_token => "#{login_token}" })
+        puts item.to_str
+        puts "Response status: #{item.code}"
+        getitemid = JSON.parse(item)
+        itemid = "#{getitemid["id"]}"
+        puts "Item ID is: #{itemid}"
+      end
+
       # Extract
       for term in @agg_dcterms
-        if term.to_s() =~ /<(.*?)\s.*">\n\s*<.*>(.*)<.*>\n\s*.*\n\s*<.*>/ then  #extract patterns <dcterms:creator attribute="something"><foaf:name>something</foaf:name></dcterms:creator>, where the element has sub-element
+        # extract patterns <dcterms:creator attribute="something"><foaf:name>something</foaf:name></dcterms:creator>, where the element has sub-element
+        if term.to_s() =~ /<(.*?)\s.*">\n\s*<.*>(.*)<.*>\n\s*.*\n\s*<.*>/ then
           name =       ($1).to_s
           name_value = ($2).to_s
-        elsif term.to_s() =~ /^<(.*?)\s.*>(.*)<.*/ and $1.to_s()!= "" then   #extract patterns like <dcterms:title attribute="something">...</dcterms:title>, where the element has attribute
+
+          # extract patterns like <dcterms:title attribute="something">...</dcterms:title>, where the element has attribute
+        elsif term.to_s() =~ /^<(.*?)\s.*>(.*)<.*/ and $1.to_s()!= "" then
           name =       ($1).to_s
           name_value = ($2).to_s
-        elsif term.to_s() =~  /^<(.*?)>(.*)<.*/ and $1.to_s()!= "" then  #extract patterns like <dcterms:abstract>...</dcterms:abstract>, where there is no attribute of element
+
+          # extract patterns like <dcterms:abstract>...</dcterms:abstract>, where there is no attribute of element
+        elsif term.to_s() =~  /^<(.*?)>(.*)<.*/ and $1.to_s()!= "" then
           name =       ($1).to_s
           name_value = ($2).to_s
         end
@@ -49,7 +94,7 @@ module DspaceOrePackager
       # puts @value
 
 
-      @terms = "'["
+      @terms = "["
       len = @key.length - 1
       for i in 0..len
         @key[i].sub!(':','.')
@@ -58,24 +103,24 @@ module DspaceOrePackager
       end
 
       @terms = @terms.chop
-      @terms += "]'"
+      @terms += "]"
 
-      puts @terms
+      puts @terms.to_s
 
+      #Update metadata
+      puts 'Updating item metadata'
+      begin
+        metadata = RestClient.put("#{@host}/rest/items/#{itemid}/metadata", "#{@terms}",
+                                  {:content_type => 'application/json', :accept => 'application/json', :rest_dspace_token => "#{login_token}" })
+        puts "Response status: #{metadata.code}"
+      end
+    end
 
-
-      # Works for single occurrence of an element as index always returns the value of the first object in self
-      # @a = @key.map{|e| {:key => e, :language => "en_US", :value => @value[@key.index(e)]}}
-      # puts @a.to_json
-
-
-
+    # Aggregated resources
+    def processAR(ar)
       # @ars.each do |aggResource|
       #   processAR(aggResource)
       # end
-    end
-    def processAR(ar)
-      puts "this doesn't do anything yet"
     end
   end
 end
